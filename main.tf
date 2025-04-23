ab491cb88acf23ed27dad84d8a7dded54ed9ebf1
TSRINEVAS <47901143+TSRINEVAS@users.noreply.github.com>
2025-04-23 17:23:30 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
 resource "google_storage_bucket" "srinivas_bucket" {
   name     = "srinivasthota1981"
   location = "US"
