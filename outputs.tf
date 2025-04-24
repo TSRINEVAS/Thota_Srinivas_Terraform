1b21a6756c21077372c24ead8d62d0d97257e064
TSRINEVAS <47901143+TSRINEVAS@users.noreply.github.com>
2025-04-24 11:42:12 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,1 @@
-output "bucket_name" {
-  description = "The name of the created GCS bucket"
-  value       = google_storage_bucket.srinivas_bucket.name
-}
 
-output "bucket_url" {
-  description = "The URL to access the GCS bucket"
-  value       = "gs://${google_storage_bucket.srinivas_bucket.name}"
-}
-
-output "bucket_location" {
-  description = "Location of the bucket"
-  value       = google_storage_bucket.srinivas_bucket.location
-}
-
-output "bucket_self_link" {
-  description = "Self link of the bucket"
-  value       = google_storage_bucket.srinivas_bucket.self_link
-}
