47583c1289ece89e51313096d5944254ee97ec61
TSRINEVAS <47901143+TSRINEVAS@users.noreply.github.com>
2025-04-24 11:40:46 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,25 +3,6 @@ provider "google" {
   region  = var.region
 }
 
-resource "google_storage_bucket" "srinivas_bucket" {
-  name                        = "srinivasthota1981"
-  location                    = "US"
-  force_destroy               = true
-  uniform_bucket_level_access = true
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle_rule {
-    action {
-      type = "Delete"
-    }
-    condition {
-      age = 365
-    }
-  }
-}
 module "network" {
   source       = "./modules/network"  # or the correct relative/remote path
   vpc_name     = var.vpc_name
