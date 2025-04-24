3bacf10bc475452e5b2214fe446c6f03fbc26d9c
TSRINEVAS <47901143+TSRINEVAS@users.noreply.github.com>
2025-04-24 10:16:10 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,3 +10,22 @@ variable "gcp_zone" {
     description = "The primary GCP zone within the region to be used"
     default = "us-west1-a"
 }
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+  default     = "my-custom-vpc"
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+  default     = "my-subnet"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR range for the subnet"
+  type        = string
+  default     = "10.0.0.0/24"
+}
+
